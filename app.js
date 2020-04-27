15d489b0859d76bbc64177d639eef2939221e032
morfeii <morfeii88@gmail.com>
2020-04-27 16:20:38 +0300

    fix deploy config

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 /* eslint-disable no-console */
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const config = require('config');
 const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use(cors());
 
 app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./routes/auth.routes'));
@@ -29,10 +24,13 @@ const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get('mongoUri'), {
+    await mongoose.connect(process.env.MONGODB_URI || config.get('mongoUri'), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
+    });
+    mongoose.connection.on('connected', () => {
+      console.log('Mongoose is connectrd!');
     });
     app.listen(PORT, () => console.log(`App has been started on port ${PORT}`));
   } catch (e) {
